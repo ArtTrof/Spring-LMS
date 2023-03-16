0c2a18297b594314c87118c91d44c113c819d78a
Artem Trofymenko <randallsng@gmail.com>
2023-03-16 13:13:43 +0100

    changed class itemRepository

--- SpringData/src/main/java/com/homework/SpringData/repository/CustomerRepository.java
+++ SpringData/src/main/java/com/homework/SpringData/repository/CustomerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedNativeQuery;
 import java.util.Optional;
 
 @Repository
@@ -13,7 +14,7 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Optional<Customer> findCustomerById(int id);
     Optional<Customer>findCustomerByUsernameStartingWith(String statement);
     @Modifying
-    @Query("delete Customer c where c.age<18")
+    @Query(value="delete from Customer where age<18",nativeQuery = true)
     void deleteCustomerDueToAge();
 
 }
